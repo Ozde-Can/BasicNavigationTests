842f5cdfa8483b1d5a3e4a857d35f3023f154335
ZeynepOz <z.ozdecan@hotmail.com>
2019-12-21 06:49:15 -0500

    my first commit

---
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -0,0 +1,21 @@
+package com.cbt.utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class BrowserFactory {
+
+    public static WebDriver getDriver() {
+       //for demo
+       //I didn't put any code yet
+       //to check which os is currently used:
+       String os = System.getProperty("os.name");
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+        String os = System.getProperty("os.name");
+        System.out.println(os);
+    }
+
+}
